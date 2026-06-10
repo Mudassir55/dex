309f9b0c249a63fb648cfae1d845ab295e7439a4
Mudassir Hassan <91305738+Mudassir55@users.noreply.github.com>
2026-06-11 01:47:56 +0500

    fix: webpack 5 polyfills

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,30 @@
+const webpack = require('webpack')
+
 module.exports = {
-  // Disable ESLint during production build to prevent deployment failures
-  eslint: {
-    enable: false,
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        path: require.resolve('path-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer'),
+        process: require.resolve('process/browser'),
+        os: require.resolve('os-browserify/browser'),
+        fs: false,
+        net: false,
+        tls: false,
+      }
+
+      webpackConfig.plugins = [
+        ...webpackConfig.plugins,
+        new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+          process: 'process/browser',
+        }),
+      ]
+
+      return webpackConfig
+    },
   },
 }
