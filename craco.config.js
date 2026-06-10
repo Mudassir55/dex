3304f255fdd51f75c1417b4b2e5c1993505e5121
Mudassir Hassan <91305738+Mudassir55@users.noreply.github.com>
2026-06-11 01:54:59 +0500

    Your commit message here

--- craco.config.js
+++ craco.config.js
@@ -1,30 +1,12 @@
-const webpack = require('webpack')
-
 module.exports = {
+  // ... your other craco configurations
   webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.resolve.fallback = {
-        ...webpackConfig.resolve.fallback,
-        path: require.resolve('path-browserify'),
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        buffer: require.resolve('buffer'),
-        process: require.resolve('process/browser'),
-        os: require.resolve('os-browserify/browser'),
-        fs: false,
-        net: false,
-        tls: false,
-      }
-
-      webpackConfig.plugins = [
-        ...webpackConfig.plugins,
-        new webpack.ProvidePlugin({
-          Buffer: ['buffer', 'Buffer'],
-          process: 'process/browser',
-        }),
-      ]
-
-      return webpackConfig
+    configure: {
+      resolve: {
+        fallback: {
+          path: false,
+        },
+      },
     },
   },
 }
